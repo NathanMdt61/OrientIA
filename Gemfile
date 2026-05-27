12ca099deae37af23c5b562bb56e7696e0670b32
Guillaume Salles <229724601+guillaume33-art@users.noreply.github.com>
2026-05-27 12:33:14 +0200

    Gros changements

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem "jbuilder"
 gem "devise"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
-
+gem "ruby_llm", "~> 1.2.0"
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,21 +1,23 @@
 class ChatsController < ApplicationController
-
-  def show
-    @chat = Chat.find(params[:id])
-    @message = Message.new
-  end
-
   def index
     @chats = Chat.all
   end
 
+  def show
+    @chat = User.first.chats.find(params[:id])
+    @message = Message.new
+  end
 
   def new
     @chat = Chat.new
   end
 
   def create
-    
+    @chat = User.first.chats.new
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-
 end
