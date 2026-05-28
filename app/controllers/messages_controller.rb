2f9f5794ffe34142ed61181bf52924ac94434daa
Guillaume Salles <229724601+guillaume33-art@users.noreply.github.com>
2026-05-28 12:53:29 +0200

    modif navbar et turbo plus d'auto scroll vers le haut

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,11 +13,17 @@ class MessagesController < ApplicationController
       @ruby_llm_chat = RubyLLM.chat
       build_conversation_history
       response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
-      Message.create(role: "assistant", content: response.content, chat: @chat)
+      @assistant_message = Message.create(role: "assistant", content: response.content, chat: @chat)
 
-      redirect_to chat_path(@chat)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to chat_path(@chat) }
+      end
     else
-      render "chats/show", status: :unprocessable_entity
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.update("new_message_container", partial: "messages/form", locals: { chat: @chat, message: @message }) }
+        format.html { render "chats/show", status: :unprocessable_entity }
+      end
     end
   end
 
